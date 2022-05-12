4106af30cfda805af731c67b5a18ea6affefc965
Audun Meltzer <audun.meltzer@gmail.com>
2022-05-12 14:14:11 +0200

    Set xs:date to ISO_LOCAL_DATE

--- fiks-arkiv-forenklet-arkivering/src/test/kotlin/no/ks/fiks/io/arkiv/model/ArkivmeldingTest.kt
+++ fiks-arkiv-forenklet-arkivering/src/test/kotlin/no/ks/fiks/io/arkiv/model/ArkivmeldingTest.kt
@@ -2,6 +2,7 @@ package no.ks.fiks.io.arkiv.model
 
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
 import mu.KotlinLogging
 import no.ks.fiks.io.arkiv.model.arkivmelding.RegistreringArkivmeldingBuilder
 import no.ks.fiks.io.arkiv.model.arkivstruktur.EksternNoekkelBuilder
@@ -15,6 +16,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.io.File
 import java.io.StringWriter
+import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.util.*
 import javax.xml.XMLConstants
@@ -27,8 +29,13 @@ class ArkivmeldingTest {
 
     @Test
     fun `Test gyldig arkivmelding`() {
+        val mottattDato = ZonedDateTime.of(2022, 10, 12, 10, 15, 11, 100, ZoneId.of("Europe/Oslo") )
+
         val registrering =
             JournalpostBuilder()
+                .dokumentetsDato(mottattDato)
+                .offentlighetsvurdertDato(ZonedDateTime.now())
+                .mottattDato(mottattDato)
                 .journalstatus(JournalStatus.GODKJENT_AV_LEDER)
                 .journalposttype(JournalpostType.UTGAENDE_DOKUMENT)
                 .journaldato(ZonedDateTime.now())
@@ -65,6 +72,9 @@ class ArkivmeldingTest {
         arkivmelding.marshal(sw)
         val xmlContent = sw.toString()
         logger.info { xmlContent }
+
+        xmlContent shouldContain "<dokumentetsDato>2022-10-12</dokumentetsDato>"
+        xmlContent shouldContain "<mottattDato>2022-10-12T10:15:11.0000001+02:00</mottattDato>"
 
         val schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
         val schema = schemaFactory.newSchema(File("target/schemas/v1/arkivmelding.xsd"))
