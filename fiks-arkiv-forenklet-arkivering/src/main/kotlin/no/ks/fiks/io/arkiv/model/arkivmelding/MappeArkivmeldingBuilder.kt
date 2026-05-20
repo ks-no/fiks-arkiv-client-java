edae0b68c109a9c3d989dee4fd93ff4c2b7bfeb3
Eivind Bjørkelund <eivind.bjorkelund@ksdigital.no>
2026-05-20 11:18:23 +0200

    Utvider arkivmeldingbuilder til å ha støtte for regler, mappe og registrering

--- fiks-arkiv-forenklet-arkivering/src/main/kotlin/no/ks/fiks/io/arkiv/model/arkivmelding/MappeArkivmeldingBuilder.kt
+++ fiks-arkiv-forenklet-arkivering/src/main/kotlin/no/ks/fiks/io/arkiv/model/arkivmelding/MappeArkivmeldingBuilder.kt
@@ -1,19 +1,3 @@
 package no.ks.fiks.io.arkiv.model.arkivmelding
 
-import no.ks.fiks.arkiv.v1.arkivmelding.opprett.Arkivmelding
-import no.ks.fiks.arkiv.v1.arkivmelding.opprett.Mappe
-
-class MappeArkivmeldingBuilder: ArkivmeldingBuilder() {
-
-    var mappe: Mappe = Mappe()
-        private set
-
-    fun mappe(mappe: Mappe) = apply { this.mappe = mappe }
-
-    override fun build(): Arkivmelding {
-        return super.build().also {
-            it.mappe = mappe
-        }
-    }
-
-}
+class MappeArkivmeldingBuilder: ArkivmeldingBuilder()
--- fiks-arkiv-forenklet-arkivering/src/main/kotlin/no/ks/fiks/io/arkiv/model/arkivmelding/RegistreringArkivmeldingBuilder.kt
+++ fiks-arkiv-forenklet-arkivering/src/main/kotlin/no/ks/fiks/io/arkiv/model/arkivmelding/RegistreringArkivmeldingBuilder.kt
@@ -1,18 +1,3 @@
 package no.ks.fiks.io.arkiv.model.arkivmelding
 
-import no.ks.fiks.arkiv.v1.arkivmelding.opprett.Arkivmelding
-
-class RegistreringArkivmeldingBuilder: ArkivmeldingBuilder() {
-
-    lateinit var registrering: IRegistrering
-        private set
-
-    fun registrering(registrering: IRegistrering) = apply { this.registrering = registrering }
-
-    override fun build(): Arkivmelding {
-        return super.build().also {
-            it.registrering = this.registrering.build()
-        }
-    }
-
-}
+class RegistreringArkivmeldingBuilder: ArkivmeldingBuilder()
